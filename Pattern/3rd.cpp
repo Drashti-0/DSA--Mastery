ac1c7479aa1ece8d3d72f8a68c4fed5bba0a06f4
Drashti_Bhanderi03 <25cs003@charusat.edu.in>
2026-07-13 23:23:17 +0530

    add new

---
+++ Pattern/3rd.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+
+   int n=5;
+   for(int i=0;i<n;i++){
+
+       char a='A';
+    for(int j=0;j<n;j++){
+        cout<<" "<<a;
+        a=a+1;
+    }
+cout<<endl;
+   }
+
+}
