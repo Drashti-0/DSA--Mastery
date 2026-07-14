326028f0f20943f9043504d06c03a3eab1af70b8
Drashti_Bhanderi03 <25cs003@charusat.edu.in>
2026-07-14 22:46:43 +0530

    add new

---
+++ Pattern/1st.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+int a=5;
+
+for(int i=0;i<a;i++){
+    for(int j=0;j<a;j++){
+       cout<< j;
+    }
+    cout<<endl;
+}
+
+cout<<endl;
+cout<<endl;
+
+
+for(int i=0;i<a;i++){
+    for(int j=0;j<a;j++){
+       cout<< "* ";
+    }
+    cout<<endl;
+}
+
+
+
+
+}
