864c2a626d8c7376ff6833c53e499de25667ef10
Drashti_Bhanderi03 <25cs003@charusat.edu.in>
2026-07-25 22:07:39 +0530

    add witrh some change

---
+++ converts/count_digit.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int count(int n){
+    if(n==0){
+        return 0;
+    }
+
+    return 1+count(n/10);
+
+}
+
+int main() {
+    // your code here
+
+    cout<<count(12345);
+    return 0;
+}
