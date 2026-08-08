b8cf5d1210287e1bb10a7035ad72604f7dccb0a5
Drashti_Bhanderi03 <25cs003@charusat.edu.in>
2026-08-08 21:01:26 +0530

    add file

---
+++ link list/printll.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+struct Node{
+     int data;
+    Node* next;
+
+};
+
+int main() {
+   
+    Node* a =new Node{10,NULL};
+    Node* b =new Node{20,NULL};
+    Node* c =new Node{30,NULL};
+
+    a->next=b;
+    b->next=c;
+
+   Node* temp = a;
+    while(temp!=NULL) {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+
+    return 0;
+}
+
+
